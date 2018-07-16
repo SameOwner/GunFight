363311e075c60d3f86acc12f509190da2ec7e7ef
Nozawa <n.shota1115@gmail.com>
2018-07-16 11:49:34 +0900

    建物の当たり判定追加 #76 #79

--- Game/js/Block/Block.js
+++ Game/js/Block/Block.js
@@ -6,8 +6,8 @@ var Block = function (core, pos, w, h) {
     this.sprite.y = pos.y;
     this.core = core;
     this.pos = new Vector2(0, 0);
-    this.pos.x = pos.x + w / 2;
-    this.pos.y = pos.y + h / 2;
+    this.pos.x = pos.x;
+    this.pos.y = pos.y;
     this.w = w;
     this.h = h;
 }
@@ -18,20 +18,21 @@ Block.prototype = {
     getPosition: function () {
         return new Vector2(this.pos.x, this.pos.y);
     },
-    IntersectCircle: function (pos, radius) {
+    IntersectCircle: function (pos, offset, radius) {
         var result = new Vector2(pos.x, pos.y);
+        result.plus(offset);
         var Left = this.pos.x - radius;
         var Right = this.pos.x + this.w + radius;
         var Up = this.pos.y - radius;
         var Down = this.pos.y + this.h + radius;
-        if (Left + radius < pos.x && pos.x < Right - radius && Up < pos.y && pos.y < Down) {
-            result.y = (pos.y - Up < Down - pos.y) ? Up : Down;
-        } else if (Up + radius < pos.y && pos.y < Down - radius && Left < pos.x && pos.x < Right) {
-            result.x = (pos.x - Left < Right - pos.x) ? Left : Right;
+        if (Left + radius < result.x && result.x < Right - radius && Up < result.y && result.y < Down) {
+            result.y = (result.y - Up < Down - result.y) ? Up : Down;
+        } else if (Up + radius < result.y && result.y < Down - radius && Left < result.x && result.x < Right) {
+            result.x = (result.x - Left < Right - result.x) ? Left : Right;
         } else {
             var p = this.getPosition();
-            p.x + (this.pos.x < pos.x) ? this.w / 2 : -this.w / 2;
-            p.y + (this.pos.y < pos.y) ? this.h / 2 : -this.h / 2;
+            p.x += (this.pos.x < result.x) ? this.w : 0;
+            p.y += (this.pos.y < result.y) ? this.h : 0;
             result.minus(p);
             if (result.length() < radius) {
                 result = result.normalize();
@@ -40,6 +41,7 @@ Block.prototype = {
             }
             result.plus(p);
         }
+        result.minus(offset);
         return result;
-    }
+    },
 }
--- Game/js/Scene/GamePlayScene.js
+++ Game/js/Scene/GamePlayScene.js
@@ -7,11 +7,15 @@ var gamePlayScene=function(core){
   var bulletManager=new BulletManager(core);
   scene.addChild(timeUi.getLabel());
 
-  var block = new Block(core, new Vector2(0, 0), 50, 50);
-  scene.addChild(block.getSprite());
+  var blockManager = new BlockManager(scene);
+  blockManager.AddBlock(new Block(core, new Vector2(0, 0), 5, 320));
+  blockManager.AddBlock(new Block(core, new Vector2(315, 0), 5, 320));
+  blockManager.AddBlock(new Block(core, new Vector2(0, 0), 320, 5));
+  blockManager.AddBlock(new Block(core, new Vector2(0, 315), 320, 5));
+  blockManager.AddBlock(new Block(core, new Vector2(50, 50), 50, 50));
 
   //プレイヤー追加
-  var player=new Player(core,new Vector2(128,128));
+  var player = new Player(core, new Vector2(128, 128));
   scene.addChild(player.getSprite()); // 現在のシーンに熊を追加
 
   //プレイヤーが弾を撃つイベント
@@ -24,7 +28,10 @@ var gamePlayScene=function(core){
   scene.addEventListener(Event.ENTER_FRAME, function(e)
   {
     player.upDate();
-    player.setPosition(block.IntersectCircle(player.getPosition(),25));
+
+    let offset = new Vector2(player.getSprite().width / 2, player.getSprite().height / 2);
+    player.setPosition(blockManager.Intersect(player.getPosition(), offset, offset.x * player.getSprite().scaleX));
+
     bulletManager.upDate();
     timeUi.upDate();
     //あたり判定テスト
